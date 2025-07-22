a0a1176e309917a2ab12f9d397a1e8ba4c87ebc4
Arju-Hasan <rajuahmeed698@gmail.com>
2025-07-22 14:33:44 +0600

    add a new section

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         "raju": "#1fb6ff",
+//         "saju": "#ff49db",
+//         "kaju": "#ff7849",
+//         "naju": "#13ce66",
+//         "maju": "#8492a6",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "custom-blue": "#1fb6ff",
+        "custom-pink": "#ff49db",
+        "custom-orange": "#ff7849",
+        "custom-green": "#13ce66",
+        "custom-gray": "#8492a6",
+      },
+    },
+  },
+};
